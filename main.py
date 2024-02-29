c3b2ba0995aede6d57c210a4072163be335b4995
anupchaturvedi1 <ac.chaturvedi@gmail.com>
2024-02-29 10:07:03 -0600

    added the Cred.py firle values as comment.

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from API.travel_functions import api_login, retrieve_travel_info, get_airport_li
 
 # DISCLAIMER
 # Need to create a creds.py file within the API folder with the following keys and your own unique values
-
+# api_key: ADD YOUR API KEY HERE
+# api_secret: ADD YOUR API SECRET
 
 if __name__ == "__main__":
     headers = api_login()
